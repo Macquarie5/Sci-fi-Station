d5b1f6e9a597a59489b80516725cf4943f74f630
Ryan Oldenhove <macquarie5@hotmail.com>
2019-08-08 18:18:52 +0930

    Init Commit

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool doorOpened = false;
+    private bool status;
+    public DoorControl controlPanel;
+    private Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+
+        status = controlPanel.GetComponent<DoorControl>().pressed;
+        Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        doorOpened = controlPanel.GetComponent<DoorControl>().pressed;
+        if (doorOpened == true)
+        {
+            Anim.SetBool("Door Open", true);
+        }
+        else
+        {
+            Anim.SetBool("Door Open", false);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour {
+
+    public bool pressed = false;
+    private Light indicator;
+
+	// Use this for initialization
+	void Start () {
+
+        indicator = GetComponentInChildren<Light>();
+        indicator.color = Color.red;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pressed)
+        {
+            indicator.color = Color.green;
+        }
+        else
+        {
+            indicator.color = Color.red;
+        }
+		
+	}
+}
